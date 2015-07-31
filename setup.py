197016e91320d9ce5932e0544413c9b416ad8148
Calvin Giles <calvin.giles@gmail.com>
2015-07-31 23:12:22 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='cosine_transform',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='calvin',
+    author_email='calvin.giles@gmail.com',
+    description='Transform a vector to a new vector a given cosine distance away.',
+    install_requires=['numpy>=1.8.0', 'scipy>=0.13.2'],
+)
