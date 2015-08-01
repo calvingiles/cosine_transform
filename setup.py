107462f1bf3af0e498804b1b01dd33aa05cab709
Calvin Giles <calvin.giles@gmail.com>
2015-08-01 21:02:04 +0100

    specify tests dir

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ setup(
     author_email='calvin.giles@gmail.com',
     description='Transform a vector to a new vector a given cosine distance away.',
     install_requires=['numpy>=1.8.0', 'scipy>=0.13.2'],
+    tests_requires=['coverage>=3.7.1', 'nosetests'],
 )
